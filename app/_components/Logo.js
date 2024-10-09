36ab30ca26ea497a43b8e3fd0cc2672cc2a72f15
Adel Yasser <100410094+dola5xd@users.noreply.github.com>
2024-10-09 16:22:23 +0300

    Update Logo.js

--- app/_components/Logo.js
+++ app/_components/Logo.js
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import Link from "next/link";
+import logoSrc from "@/public/logo.png";
 
 function Logo() {
   return (
     <Link href="/" className="flex items-center gap-4 z-10">
-      <Image height="60" width="60" src="@/public/Logo.png" alt="The Wild Oasis logo" />
+      <Image height="60" width="60" src={logoSrc} alt="The Wild Oasis logo" />
       <span className="text-xl font-semibold text-primary-100">
         The Wild Oasis
       </span>
